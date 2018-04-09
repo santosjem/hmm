74f33cc747c5c206ad3c5c4a9d0087242664a51a
Willie Kaufman <williekaufman@Willies-MacBook-Pro.local>
2018-04-09 02:59:28 -0500

    added second tests as well

--- asssignment.scm
+++ asssignment.scm
@@ -54,6 +54,57 @@
 
 ;(log-evidence t2-4 typist2 counts marg)
 
+(define ln-to-log10
+  (lambda (num)
+    (/ num (log 10))))
+
+(define skip-first-tests #t) ; prevents first tests so it runs faster
+
+; This is for the first bullet point in analysis
+; example run: (first-tests 1 1 t1-1 o1 t2-4 o4 t1-8)
+; uniform prior, training using letters 1 and 4, analyzing who wrote letter 8
+(define first-tests
+  (lambda
+      (prior-typist-1
+       prior-typist-2
+       corrupted-typist-1
+       original-typist-1
+       corrupted-typist-2
+       original-typist-2
+       to-test)
+    (if
+     skip-first-tests
+     0
+     (let ([typist-1-typo (create-counts 28 1)]
+           [typist-2-typo (create-counts 28 1)]
+           [trans (create-counts 28 1)]
+           [prior-evidence
+            (* 10 (ln-to-log10 (log (/ prior-typist-1 prior-typist-2))))])
+       (count-transitions! original-typist-1 trans) 
+       (count-transitions! original-typist-2 trans) ; train transition model
+       (count-conditionals! original-typist-1 corrupted-typist-1 typist-1-typo)
+       ; train typist 1 model
+       (count-conditionals! original-typist-2 corrupted-typist-2 typist-2-typo)
+       ; train typist 2 typo model
+       (let ([marg (marginal-counts trans)])
+         (normalize-marginal-counts! marg)
+         (normalize-counts! typist-1-typo)
+         (normalize-counts! typist-2-typo) ; normalize everything
+         (+ prior-evidence
+            (* 10 (ln-to-log10
+                   (- (log-evidence to-test typist-1-typo trans marg)
+                      (log-evidence to-test typist-2-typo trans marg)
+                      ; difference in log likelihoods for evidence calculation
+                      )))))))))
+
+
+
+
+; you never count-conditionals - I think you're going to need to do that? unless
+; I'm missing what this function was for
+; I wrote something for the first bullet point above - it was easier for me to
+; just write something from scratch to understand what it did. Which part of the
+; analysis was this for?
 (define test
   (lambda
       (corrupt
@@ -70,17 +121,205 @@
         (normalize-counts! typist-counts)
         (log-evidence corrupt typist-counts counts marg)))))
 
-(display "Training on letters 1 and 12, Typist 1 letter 2") (newline)
-(test t1-14 t1-1 t2-12 t1-2) (newline)
-(test t1-17 t1-2 t2-12 t1-2) (newline)
-(test t2-13 t1-2 t2-12 t1-2) (newline)
-(test t2-19 t1-2 t2-12 t1-2) (newline)
+;(display "Training on letters 1 and 12, Typist 1 letter 2") (newline)
+;(test t1-14 t1-1 t2-12 t1-2) (newline)
+;(test t1-17 t1-2 t2-12 t1-2) (newline)
+;(test t2-13 t1-2 t2-12 t1-2) (newline)
+;(test t2-19 t1-2 t2-12 t1-2) (newline)
 
-(display "Training on letters 1 and 12, Typist 2 letter 12") (newline)
-(test t1-14 t1-1 t2-12 t2-12) (newline)
-(test t1-17 t1-2 t2-12 t2-12) (newline)
-(test t2-13 t1-2 t2-12 t2-12) (newline)
-(test t2-19 t1-2 t2-12 t2-12) (newline)
+;(display "Training on letters 1 and 12, Typist 2 letter 12") (newline)
+;(test t1-14 t1-1 t2-12 t2-12) (newline)
+;(test t1-17 t1-2 t2-12 t2-12) (newline)
+;(test t2-13 t1-2 t2-12 t2-12) (newline)
+;(test t2-19 t1-2 t2-12 t2-12) (newline)
 
+(display "we used a uniform prior for the writers of the letters") (newline)
+(display "Training on letters 1 and 4, testing letter 8") (newline)
+(first-tests 1 1 t1-1 o1 t2-4 o4 t1-8)
+(display "Training on letters 1 and 4, testing letter 16") (newline)
+(first-tests 1 1 t1-1 o1 t2-4 o4 t1-16)
+(display "Training on letters 1 and 4, testing letter 9") (newline)
+(first-tests 1 1 t1-1 o1 t2-4 o4 t2-9)
+(display "Training on letters 1 and 4, testing letter 18") (newline)
+(first-tests 1 1 t1-1 o1 t2-4 o4 t2-18)
 
+(newline)
+
+(display "Training on letters 8 and 4, testing letter 1") (newline)
+(first-tests 1 1 t1-8 o8 t2-4 o4 t1-1)
+(display "Training on letters 8 and 4, testing letter 16") (newline)
+(first-tests 1 1 t1-8 o8 t2-4 o4 t1-16)
+(display "Training on letters 8 and 4, testing letter 9") (newline)
+(first-tests 1 1 t1-8 o8 t2-4 o4 t2-9)
+(display "Training on letters 8 and 4, testing letter 18") (newline)
+(first-tests 1 1 t1-8 o8 t2-4 o4 t2-18)
+
+(newline)
+
+(display "Training on letters 16 and 4, testing letter 1") (newline)
+(first-tests 1 1 t1-16 o16 t2-4 o4 t1-1)
+(display "Training on letters 16 and 4, testing letter 8") (newline)
+(first-tests 1 1 t1-16 o16 t2-4 o4 t1-8)
+(display "Training on letters 16 and 4, testing letter 9") (newline)
+(first-tests 1 1 t1-16 o16 t2-4 o4 t2-9)
+(display "Training on letters 16 and 4, testing letter 18") (newline)
+(first-tests 1 1 t1-16 o16 t2-4 o4 t2-18)
+
+(newline)
+
+(display "Training on letters 1 and 9, testing letter 8") (newline)
+(first-tests 1 1 t1-1 o1 t2-9 o9 t1-8)
+(display "Training on letters 1 and 9, testing letter 16") (newline)
+(first-tests 1 1 t1-1 o1 t2-9 o9 t1-16)
+(display "Training on letters 1 and 9, testing letter 4") (newline)
+(first-tests 1 1 t1-1 o1 t2-9 o9 t2-4)
+(display "Training on letters 1 and 9, testing letter 18") (newline)
+(first-tests 1 1 t1-1 o1 t2-9 o9 t2-18)
+
+(newline)
+
+(display "Training on letters 8 and 9, testing letter 1") (newline)
+(first-tests 1 1 t1-8 o8 t2-9 o9 t1-1)
+(display "Training on letters 8 and 4, testing letter 16") (newline)
+(first-tests 1 1 t1-8 o8 t2-9 o9 t1-16)
+(display "Training on letters 8 and 4, testing letter 4") (newline)
+(first-tests 1 1 t1-8 o8 t2-9 o9 t2-4)
+(display "Training on letters 8 and 4, testing letter 18") (newline)
+(first-tests 1 1 t1-8 o8 t2-9 o9 t2-18)
+
+(newline)
+
+(display "Training on letters 16 and 9, testing letter 1") (newline)
+(first-tests 1 1 t1-16 o16 t2-9 o9 t1-1)
+(display "Training on letters 16 and 9, testing letter 8") (newline)
+(first-tests 1 1 t1-16 o16 t2-9 o9 t1-8)
+(display "Training on letters 16 and 9, testing letter 4") (newline)
+(first-tests 1 1 t1-16 o16 t2-9 o9 t2-4)
+(display "Training on letters 16 and 9, testing letter 18") (newline)
+(first-tests 1 1 t1-16 o16 t2-9 o9 t2-18)
+
+(newline)
+
+(display "Training on letters 1 and 18, testing letter 8") (newline)
+(first-tests 1 1 t1-1 o1 t2-18 o9 t1-8)
+(display "Training on letters 1 and 18, testing letter 16") (newline)
+(first-tests 1 1 t1-1 o1 t2-18 o9 t1-16)
+(display "Training on letters 1 and 18, testing letter 4") (newline)
+(first-tests 1 1 t1-1 o1 t2-18 o9 t2-4)
+(display "Training on letters 1 and 18, testing letter 9") (newline)
+(first-tests 1 1 t1-1 o1 t2-18 o9 t2-9)
+
+(newline)
+
+(display "Training on letters 8 and 18, testing letter 1") (newline)
+(first-tests 1 1 t1-8 o8 t2-18 o18 t1-1)
+(display "Training on letters 8 and 18, testing letter 16") (newline)
+(first-tests 1 1 t1-8 o8 t2-18 o18 t1-16)
+(display "Training on letters 8 and 18, testing letter 4") (newline)
+(first-tests 1 1 t1-8 o8 t2-18 o18 t2-4)
+(display "Training on letters 8 and 18, testing letter 9") (newline)
+(first-tests 1 1 t1-8 o8 t2-18 o18 t2-9)
+
+(newline)
+
+(display "Training on letters 16 and 18, testing letter 1") (newline)
+(first-tests 1 1 t1-16 o16 t2-18 o18 t1-1)
+(display "Training on letters 16 and 18, testing letter 8") (newline)
+(first-tests 1 1 t1-16 o16 t2-18 o18 t1-8)
+(display "Training on letters 16 and 18, testing letter 4") (newline)
+(first-tests 1 1 t1-16 o16 t2-18 o18 t2-4)
+(display "Training on letters 16 and 18, testing letter 9") (newline)
+(first-tests 1 1 t1-16 o16 t2-18 o18 t2-9)
+
+(define transition-model (create-counts 28 1))
+
+;;; This is maybe a really stupid way of doing this but I don't know of a
+;;; better way, not that great at program flow in scheme - doing things with
+;;; side effects is kind of weird in a functional language, I guess
+(define train-transition-model
+  (lambda (lst)
+    (if
+     (null? lst)
+     (values) ; no need to return anything
+     (count-transitions! (car lst) transition-model))
+    (if
+     (null? lst)
+     (values) ; no need to return anything
+     (train-transition-model (cdr lst)))))
+
+(define train-typo-model
+  (lambda (originals corrupted count)
+    (if
+     (null? originals)
+     (values) ; no need to return anything
+     (count-conditionals! (car originals) (car corrupted) count))
+    (if
+     (null? originals)
+     (values) ; no need to return anything
+     (train-typo-model (cdr originals) (cdr corrupted) count))))
+
+(define originals (list o1 o8 o16 o4 o9 o18))
+
+(define t1-originals (list o1 o8 o16))
+(define t2-originals (list o4 o9 o18))
+
+(define t1-corrupteds (list t1-1 t1-8 t1-16))
+(define t2-corrupteds (list t2-4 t2-9 t2-18))
+
+(train-transition-model originals)
+(define marg (marginal-counts transition-model))
+(normalize-marginal-counts! marg)
+
+(define typist-1-counts (create-counts 28 1))
+(define typist-2-counts (create-counts 28 1))
+ 
+(train-typo-model t1-originals t1-corrupteds typist-1-counts)
+(train-typo-model t2-originals t2-corrupteds typist-2-counts)
+
+(normalize-counts! typist-1-counts)
+(normalize-counts! typist-2-counts)
+(normalize-counts! transition-model)
+
+(define skip-second-tests #t) ; prevents second tests so it runs faster
+
+(define second-test
+  (lambda (prior1 prior2 letter)
+    (if
+     skip-second-tests
+     0
+     (* 10
+        (+
+         (ln-to-log10 (log (/ prior1 prior2)))
+         (ln-to-log10
+          (-
+           (log-evidence letter typist-1-counts transition-model marg)
+           (log-evidence letter typist-2-counts transition-model marg))))))))
+
+(display "uniform prior for who wrote the letter") (newline)
+(display "Training on all letters, testing letter 2 by typist 1") (newline)
+(second-test 1 1 t1-2)
+(display "Training on all letters, testing letter 14 by typist 1") (newline)
+(second-test 1 1 t1-14)
+(display "Training on all letters, testing letter 17 by typist 1") (newline)
+(second-test 1 1 t1-17)
+(display "Training on all letters, testing letter 12 by typist 2") (newline)
+(second-test 1 1 t2-12)
+(display "Training on all letters, testing letter 13 by typist 2") (newline)
+(second-test 1 1 t2-13)
+(display "Training on all letters, testing letter 19 by typist 2") (newline)
+(second-test 1 1 t2-19)
+(display "Training on all letters, testing unknown letter 5") (newline)
+(second-test 1 1 u5)
+(display "Training on all letters, testing unknown letter 6") (newline)
+(second-test 1 1 u6)
+(display "Training on all letters, testing unknown letter 20") (newline)
+(second-test 1 1 u20)
+(display "Training on all letters, testing unknown letter 21") (newline)
+(second-test 1 1 u21)
+(display "Training on all letters, testing unknown letter 23") (newline)
+(second-test 1 1 u23)
+(display "Training on all letters, testing unknown letter 24") (newline)
+(second-test 1 1 u24)
+       
+    
 
